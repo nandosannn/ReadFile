42b9e60e63753671067e72ed1358f21e8f8632e7
Fernando Santos <silvafernandosan@gmail.com>
2024-02-18 11:29:25 -0300

    Try-with-resources

--- src/App.java
+++ src/App.java
@@ -1,19 +1,14 @@
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
-import java.util.Scanner;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
         String path = "teste.txt";
-        FileReader fileReader = null;
-        BufferedReader buffer = null;
 
-        try{
-            fileReader = new FileReader(path);
-            buffer = new BufferedReader(fileReader);
+
+        try (BufferedReader buffer = new BufferedReader(new FileReader(path))){
 
             String line = buffer.readLine();
 
@@ -24,19 +19,6 @@ public class App {
         }
         catch(IOException e){
             System.out.println(e.getMessage());
-        }
-        finally{
-            try {
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-                if (buffer != null) {
-                    buffer.close();
-                }
-            }  
-            catch(IOException e){
-                e.fillInStackTrace();
-            }  
         }
     }
 
